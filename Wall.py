6960db56c24ff9946a501ef741b9d173eb576b12
Joe1310 <46935029+Joe1310@users.noreply.github.com>
2020-01-27 16:10:01 +0000

    Add files via upload

--- Wall.py
+++ Wall.py
@@ -4,6 +4,12 @@ from Sprites import Sprite
 
 class Wall(Sprite):
     def __init__(self, x, y):
-        self.xPos = x
-        self.yPos = y
-        self.width = 20
+        pygame.sprite.Sprite.__init__(self)
+        self.x = x
+        self.y = y
+        self.image = pygame.Surface([20, 20])
+        self.image.fill((0, 0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+
+
