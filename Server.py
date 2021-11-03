aba9a7e5b25baea68797b7e36a56e73ea2129883
Joe1310 <46935029+Joe1310@users.noreply.github.com>
2021-11-03 22:57:30 +0000

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,6 +1,8 @@
 import socket
 from _thread import *
 import pickle
+import random
+
 
 # Local Ip (IPV4 FROM CMD IPCONFIG, DEVICE SPECIFIC)
 server = socket.gethostbyname(socket.gethostname())
@@ -22,41 +24,68 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for connection, Server Initiated")
 
-playerPositions = [[100, 100], [400, 100]]
+newPlatY = [480]
+
+# player start positions
+playerPositions = [[200, 440], [200, 440]]
+
+# keeps track of any deaths
+dead = False
 
 
-# client = connection, addr = address
+# client = connection, player = player number
 def threaded_client(client, player):
     while True:
         data = pickle.loads(client.recv(2048))
         # checks to see if any data is being received from the client, if not it assumes that the client is
         # disconnected and stops running in the background
         if data:
+
             if data == "getPos":
                 reply = (playerPositions[player])
-            # reply with other player position
+
+            elif data == "dead":
+                dead = True
+
+            elif data == "checkdead":
+                reply = dead
+                
+	   # reply with other player position
+            elif data == "getPlayerCount":
+                reply = currentConnections
+
+            # sets platform position from client 1's generation algorithm
+            elif data[1] == "sendPlat":
+                newPlatY[0] = data[0]
+
+            # sends the new platform's y position to the client
+            elif data == "newPlat":
+                print(newPlatY[0])
+                reply = newPlatY[0]
+
+	    # sets the player positions of the client sending the data
             else:
                 if player == 0:
                     playerPositions[0] = data
-                    reply = playerPositions[1]
+                    reply = (playerPositions[1], 0)
                 else:
                     playerPositions[1] = data
-                    reply = playerPositions[0]
+                    reply = (playerPositions[0], 1)
+
         else:
             print("Lost Connection")
             client.close()
             break
 
-        # sends data back to clients
+        # sends data back to all open threads
         client.sendall(pickle.dumps(reply))
 
 
-# keeps track of current playerPositions data
+# keeps track of current number of players
 currentPlayer = 0
+currentConnections = 0
 while True:
     connection, addr = s.accept()
     print("Connected to:", addr)
     start_new_thread(threaded_client, (connection, currentPlayer))
     currentPlayer += 1
-
-# GENERATE MAP ON SERVER SIDE AND SEND PLATFORM DATA TO CLIENTS AND PUT INTO SPRITE LIST TO BE BLITTED.
